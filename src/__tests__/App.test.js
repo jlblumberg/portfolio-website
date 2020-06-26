2fd63dfeb036a1cdfb4c0b72f9b1ae9e52b5fb0b
Josh Blumberg <jlblumberg@gmail.com>
2020-06-26 11:10:28 +0100

    Add app component

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { mount } from 'enzyme';
 import App from '../App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+
+  it('renders correctly', () => {
+    const wrapper = mount(<App />);
+    expect(wrapper).toMatchSnapshot();
+  });
+
 });
