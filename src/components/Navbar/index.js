f5b3a1328576b5cd3c42a0ef14a88711e7b70131
Josh Blumberg <jlblumberg@gmail.com>
2020-06-29 12:12:20 +0100

    Add styling to navbar

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import { AppBar, Toolbar, IconButton, Button } from '@material-ui/core';
 import HomeRoundedIcon from '@material-ui/icons/HomeRounded';
+import styles from './Navbar.module.css';
 
 const Navbar = () => {
   return (
-    <div>
+    <div className={styles.root}>
       <AppBar position="static">
         <Toolbar>
           <IconButton edge="start" id='home-button'>
             <HomeRoundedIcon />
           </IconButton>
+          <div className={styles.centerPadding}></div>
           <Button>about</Button>
           <Button>experience</Button>
           <Button>work</Button>
