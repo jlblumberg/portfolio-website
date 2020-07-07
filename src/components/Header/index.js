2f74825ef39537dc0683b94cda22334a9a25d6aa
Josh Blumberg <jlblumberg@gmail.com>
2020-07-07 18:00:47 +0100

    Add content to header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import styles from './Header.module.css';
+import { Typography } from '@material-ui/core';
 
 const Header = () => {
   return(
-    <div>
-      Header
+    <div className={styles.container}>
+      <img src="https://i.imgur.com/OSV6Wuy.jpg" id="profile-picture" alt="Josh's face" className={styles.profilePic} />
+      <Typography variant='h2'>Hi.</Typography>
+      <Typography variant='h2' color='textSecondary'>I'm Josh Blumberg.</Typography>
+      <Typography variant='h2' color='textSecondary'>I build software for the web.</Typography>
     </div>
   );
 }
