c4dd04dd5c3601df327b82c4f326493e0290b611
Josh Blumberg <jlblumberg@gmail.com>
2020-07-14 12:32:18 +0100

    Start populating project card waterfall:

--- src/components/Work/index.js
+++ src/components/Work/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './Work.module.css'
-import { Typography, IconButton } from '@material-ui/core';
+import { Typography, IconButton, Card, CardActions, CardContent, CardMedia, Button } from '@material-ui/core';
+import { projects } from '../../work.js'
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
+import Masonry from 'react-masonry-css';
 
 const Work = () => {
 
@@ -9,11 +11,56 @@ const Work = () => {
     alert('add in scroll functionality later')
   }
 
-  return(
+  const columnBreakpoints = {
+    default: 3,
+    1100: 3,
+    1000: 2,
+    600: 1
+  };
+
+  const projectCards = projects.map((project, index) => (
+    <Card key={index}>
+      <CardMedia
+        component="img"
+        alt={project.alt}
+        image={project.image}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">
+          {project.title}
+        </Typography>
+        <Typography gutterBottom color="textSecondary">
+          {project.techstack.join(" / ")}
+        </Typography>
+        <Typography component="p">
+          {project.description}
+        </Typography>
+      </CardContent>
+      <CardActions className={styles.cardActions}>
+        {project.websitelink === null ? null : (
+          <Button href={project.websitelink} color="primary" variant="contained">
+            App
+          </Button>
+        )}
+        <Button href={project.githublink} color="primary" variant="contained">
+          Code
+        </Button>
+      </CardActions>
+    </Card>
+  ))
+
+  return (
     <div className={styles.flexContainer}>
       <div className={styles.headerText} id='header'>
         <Typography variant='h5' color='textSecondary'>MY WORK</Typography>
       </div>
+      <Masonry
+        breakpointCols={columnBreakpoints}
+        className={styles.masonryGrid}
+        columnClassName={styles.masonryGridColumns}
+      >
+        {projectCards}
+      </Masonry>
       <div>
         <IconButton color='primary' id='down-button' onClick={handleClick}>
           <KeyboardArrowDownIcon fontSize='large' />
---
+++ src/work.js
@@ -0,0 +1,30 @@
+export const projects = 
+[
+  {
+    title: "Project title",
+    githublink: "https://github.com/jlblumberg",
+    websitelink: "https://google.com",
+    description: "A 16-week intensive software development bootcamp, focused on object-oriented design, Agile methodologies, test-driven development, and pair programming",
+    image: "https://i.imgur.com/K2HelWY.png",
+    alt: "image alt",
+    techstack: ["Ruby", "Rails", "JavaScript"],
+  },
+  {
+    title: "Project title",
+    githublink: "https://github.com/jlblumberg",
+    websitelink: "https://google.com",
+    description: "A 16-week intensive software development bootcamp, focused on object-oriented design, Agile methodologies, test-driven development, and pair programming",
+    image: "https://i.imgur.com/K2HelWY.png",
+    alt: "image alt",
+    techstack: ["Ruby", "Rails", "JavaScript"],
+  },
+  {
+    title: "Project title",
+    githublink: "https://github.com/jlblumberg",
+    websitelink: "https://google.com",
+    description: "A 16-week intensive software development bootcamp, focused on object-oriented design, Agile methodologies, test-driven development, and pair programming",
+    image: "https://i.imgur.com/K2HelWY.png",
+    alt: "image alt",
+    techstack: ["Ruby", "Rails", "JavaScript"],
+  },
+]
